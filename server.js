00c81d6128fbfa6a5ac40a6bf4c5adcf8a9994cb
Colton Kayser <coltonkayser@gmail.com>
2019-09-16 16:50:08 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+//dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express ();
+const db = mongoose.connection;
+
+//port
+const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true});
+
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connnected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => console.log("listening on port:", PORT));
